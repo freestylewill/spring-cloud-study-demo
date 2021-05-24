a3f9002617b08212f1072e920b7f21d756d2a245
majun <freestylewill@163.com>
2021-05-24 16:07:54 +0800

    加入config的demo

---
+++ config/config-root/spring-cloud-config-server/src/test/java/com/guigu/SpringcloudConfigServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guigu;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudConfigServerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
